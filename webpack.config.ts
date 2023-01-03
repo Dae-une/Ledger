d9268665196da5306ae083d70b2ad22e6237cf73
Dae-une <ko301201@naver.com>
2023-01-03 19:09:02 +0900

    init

---
+++ webpack.config.ts
@@ -0,0 +1,71 @@
+import path from "path";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import webpack, { Configuration as WebpackConfiguration } from "webpack";
+import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+const config: Configuration = {
+  name: "ledger",
+  mode: isDevelopment ? "development" : "production",
+  devtool: !isDevelopment ? "hidden-source-map" : "eval",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+  },
+  entry: {
+    app: "./client",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
+          env: {
+            development: {
+              plugins: [require.resolve("react-refresh/babel")],
+            },
+          },
+        },
+        exclude: path.join(__dirname, "node_modules"),
+      },
+      {
+        test: /\.css?$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+    }),
+    new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? "development" : "production" }),
+  ],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+    publicPath: "/dist/",
+  },
+  devServer: {
+    historyApiFallback: true, // react router
+    port: 3090,
+    devMiddleware: { publicPath: "/dist/" },
+    static: { directory: path.resolve(__dirname) },
+  },
+};
+
+if (isDevelopment && config.plugins) {
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  config.plugins.push(new ReactRefreshWebpackPlugin());
+}
+if (!isDevelopment && config.plugins) {
+}
+
+export default config;
