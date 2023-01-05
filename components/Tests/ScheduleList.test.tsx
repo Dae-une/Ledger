005032452e57a3f0a54ca429970dd12023c85f8b
Dae-une <ko301201@naver.com>
2023-01-05 15:04:02 +0900

    test: ScheduleList

---
+++ components/Tests/ScheduleList.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { Route } from 'react-router-dom';
+import { withRouter } from '../../tests/utils';
+import { scheduleData } from '../../mocks/data';
+import ScheduleList from '../ScheduleList/ScheduleList';
+
+describe('ScheduleList', () => {
+  it('render correctly', async () => {
+    const { asFragment } = render(withRouter(<Route path="/" element={<ScheduleList list={scheduleData} />} />));
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('리스트가 없으면 Empty화면을 보여준다.', () => {
+    render(withRouter(<Route path="/" element={<ScheduleList list={[]} />} />));
+
+    expect(screen.getByText('일정이 없어요.'));
+  });
+});
